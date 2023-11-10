b9a2525684f486770ed7815a3ed6f0040c55eb10
moditha23 <102273283+moditha23@users.noreply.github.com>
2023-11-10 16:47:48 +0530

    Moditha_init2

--- index.php
+++ index.php
@@ -11,15 +11,15 @@ include('include/header.php');
     border-color: #FF00AA;
 }
 
-<h2>Damith was here</h2>
+<h2>Moditha was here</h2>
   </style>
     <div class="hero-wrap js-fullheight" style="background-image: url('images/bg_2.jpg');" data-stellar-background-ratio="0.5">
       <div class="overlay"></div>
       <div class="container">
-        <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-start" data-scrollax-parent="true">
+        <div class="row no-gutsters slider-text js-fullheight align-items-center justify-content-start" data-scrollax-parent="true">
           <div class="col-xl-10 ftco-animate mb-5 pb-5" data-scrollax=" properties: { translateY: '70%' }">
           	<!-- <p class="mb-4 mt-5 pt-5" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">We have <span class="number" data-number="850000">0</span> great job offers you deserve!</p> -->
-            <h1 class="mb-5" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Your Dream <br><span>Job is Waiting</span></h1>
+            <h1 class="mb-5" data-scrollax="properties:  ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ{ translateY: '30%', opacity: 1.6 }">Your Dream <br><span>Job is Waiting</span></h1>
 
 						<div class="ftco-search">
 							<div class="row">
