015ecd07cadd3388c17b926a0986a3e8ea6adf05
poisonleviathan <73103757+poisonleviathan@users.noreply.github.com>
2023-11-10 16:10:22 +0530

    test2

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ include('include/header.php');
     border-color: #FF00AA;
 }
 
-<h1> Hello  World Deskano</h1>
+
   </style>
     <div class="hero-wrap js-fullheight" style="background-image: url('images/bg_2.jpg');" data-stellar-background-ratio="0.5">
       <div class="overlay"></div>
